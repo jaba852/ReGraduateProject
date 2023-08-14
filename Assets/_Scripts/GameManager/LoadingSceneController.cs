7e16722db063b82f5467d2f380d8adf8fc62514d
gksgmlfkr <131153965+gksgmlfkr@users.noreply.github.com>
2023-08-14 22:48:17 +0900

    loadingSence 스크립트 추가
    loadingSence 바꿔서 쓰고 있었는데 옛날건줄 알고 지워서 문제 생긴거 같음

---
+++ Assets/_Scripts/GameManager/LoadingSceneController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingSceneController : MonoBehaviour
+{   
+    public Image progressfill;
+    void Start(){
+        if(PlayerPrefs.GetInt("sceneIndex")!= null){
+        int sceneBuildIndex = PlayerPrefs.GetInt("sceneIndex") + 1;
+        
+        StartCoroutine(LoadSceneAsyncCoroutine(sceneBuildIndex));
+        }
+        else 
+        SceneManager.LoadSceneAsync(1);
+    }
+    /*public void LoadSceneAsync(int sceneBuildIndex)
+    {  // SceneManager.LoadSceneAsync(5);
+       // StartCoroutine(LoadSceneAsyncCoroutine(sceneBuildIndex));
+        
+    }*/
+
+    private IEnumerator LoadSceneAsyncCoroutine(int sceneBuildIndex)
+    {   yield return null;
+        if (sceneBuildIndex == 1) 
+        {
+            ResetPlayerStats();
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneBuildIndex);
+        asyncLoad.allowSceneActivation = false;
+
+        float timer = 0.0f;
+
+        while (!asyncLoad.isDone)
+        {    yield return null;
+             timer += Time.deltaTime;
+             if(asyncLoad.progress<0.9f){
+             progressfill.fillAmount =  Mathf.Lerp(progressfill.fillAmount,  asyncLoad.progress, timer);
+                if(progressfill.fillAmount >=asyncLoad.progress){
+                timer = 0f;
+                }}   
+             else 
+            {
+                progressfill.fillAmount = Mathf.Lerp(progressfill.fillAmount,1f,timer);
+                if(progressfill.fillAmount == 1.0f){
+                asyncLoad.allowSceneActivation = true;
+                 PlayerPrefs.SetInt("sceneIndex",0);
+                yield break;
+                }
+            }
+            
+        
+        }
+
+    }
+    
+    public void ResetPlayerStats()
+    {
+        WarriorStatus warriorStatus = FindObjectOfType<WarriorStatus>();
+        if (warriorStatus != null)
+        {
+            warriorStatus.InitializePlayerStats();
+        }
+    }
+}
